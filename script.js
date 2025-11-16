444e0148f78c0b2e9ca9dc46eb75230a3f20ea68
MedikTV <petrskarvan@seznam.cz>
2025-11-17 00:06:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const lightbox = document.getElementById("lightbox");
     const lightboxImg = document.getElementById("lightbox-img");
+    const imgs = document.querySelectorAll(".project-img, .setup-img");
 
-    document.querySelectorAll(".project-img").forEach(img => {
+    imgs.forEach(img => {
         img.addEventListener("click", () => {
             lightboxImg.src = img.src;
             lightbox.style.display = "flex";
